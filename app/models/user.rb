aa5690c270f54c0164eb2ea7254fe1d7b7a6795d
Soon-Mi <ssoonmi@gmail.com>
2018-10-21 15:52:11 -0400

    Change password length minimum from 7 to 6

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@
 class User < ApplicationRecord
   validates :email, :session_token, presence: true, uniqueness: true
   validates :password_digest, presence: true
-  validates :password, length: { minimum: 7 }, allow_nil: true
+  validates :password, length: { minimum: 6 }, allow_nil: true
 
   after_initialize :ensure_session_token
 
